2d0a322a6a03d5bbcf95c5e4db6e9a963b37fffd
allan-gilber <allangilber@yahoo.com.br>
2022-10-03 17:13:36 -0300

    fix: removed unecessary funtions.

--- src/data/UsersData/UsersData.ts
+++ src/data/UsersData/UsersData.ts
@@ -1,17 +1,12 @@
-// import {productPropertiesObject} from '../../model/ProductsModels';
 import DataBase from '../../services/DataBase';
 
 export default class UsersData extends DataBase {
-  async checkIfProductExists(productName: string){
-    return await this.connection().table('products').select('*').where({product_name: productName});
-  }
-
   async getUsersList(){
     const response = await this.connection().table('users').select('user_name', 'user_email', 'user_birthday', 'user_phone_number').orderBy('user_id', 'asc');
     return response;
   }
 
-  async insertNewUsersProducts(userName: string, userEmail: string, userBirthday: string, userPhoneNumber: string): Promise<any> {
+  async insertNewUsers(userName: string, userEmail: string, userBirthday: string, userPhoneNumber: string): Promise<any> {
     const response = await this.connection().table('users').insert({
       user_name: userName,
       user_email: userEmail,
