3b82fc2247d9d07cc16af818cdbd2d5d7e465ca0
allan-gilber <allangilber@yahoo.com.br>
2022-10-22 01:11:27 -0300

    fix: fixed path to migration

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import {Request, Response} from 'express';
 import {UsersController} from './application/usersController/usersController';
 import app from './business/applicationBusiness/applicationLogic';
 
-
 // GET
 /* Simple endpoint to verify if server is online */
 app.get('/', (req: Request,res: Response) => res.status(200).send({message: 'server online.'}));
