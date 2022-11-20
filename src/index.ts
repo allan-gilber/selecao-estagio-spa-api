ff47b2bc99889219d3d31e4b6c1ad3d46d0fe11b
allan-gilber <allangilber@yahoo.com.br>
2022-11-20 03:53:42 -0300

    feat: inserted package axios and set first getendpoint

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
 import {Request, Response} from 'express';
-import {UsersController} from './application/usersController/usersController';
 import app from './business/applicationBusiness/applicationLogic';
 
 // GET
 /* Simple endpoint to verify if server is online */
-app.get('/', (req: Request,res: Response) => res.status(200).send({message: 'server online.'}));
-/* Returns all users. */
-app.get('/users', async (req: Request,res: Response) => await new UsersController().getUsersList(res));
-
-// POST
-/* Create new user. */
-app.post('/users', async (req: Request,res: Response) => await new UsersController().insertNewUsers(req, res));
+app.get('/', (req: Request,res: Response) => res.status(200).send({message: 'Fullstack Challenge 🏅 - Space X API'}));
