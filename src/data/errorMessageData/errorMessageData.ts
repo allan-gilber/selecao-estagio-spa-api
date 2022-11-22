2c3304b7ce19258eb10206235ee6c8c35ee6e8ce
allan-gilber <allangilber@yahoo.com.br>
2022-11-22 06:48:58 -0300

    fix: cleaned unecessary error messages.

--- src/data/errorMessageData/errorMessageData.ts
+++ src/data/errorMessageData/errorMessageData.ts
@@ -3,14 +3,13 @@ import {errorMessageObject} from '../../model/errorMessageModels';
 export const errorMessagesData: errorMessageObject  = {
   genericError: {status: 500, message:'oops, something went wrong!'},
   // empty/incomplete
-  emptyParamtersForNewUser: {status: 400, message:'please, provide a valid paramters. [name: string (length <= 3), email: string, birthday: date, phonenumber: string]'},
   // invalid
-  invalidParamterForUser: {status: 400, message:'please, provide a valid phone number for the user. (max length: 11)'},
-  invalidParamterForBirthday: {status: 400, message:'please, provide a valid date for birthday. (YYYY-MM-DD)'},
-  invalidPhoneNumber: {status: 400, message:'please, provide valid phone number. (string, length = 11)'},
-  invalidParamterForEmail: {status: 400, message:'please, provide valid email.'},
   // successful
-  successfullyInsertedNewUsers: {status: 201, message: 'succesfully registered new User!'},
   // database errors
-  ER_DUP_ENTRY: {status:400,message:'Email already in use.'}
+  ER_DUP_ENTRY_FOR_POPULATING_ROCKETS_TABLE: {message:'duplicated entry for rockets table.'},
+  // Table errors
+  ROCKETS_TABLE_ALREADY_EXISTS: {message: 'createRocketsTable: failure to created new table (table already exists). Proceeding to next task...'},
+  LAUNCHS_TABLE_ALREADY_EXISTS: {message: 'createLaunchsTable: failure to created new table (table already exists). Proceeding to next task...'},
+  // Populate errors
+  EMPTY_RESPONSE_FOR_ROCKETS_DATA_GRAB: {message: 'populateRocketsTable: request returned empty. Third-party API may be offline.'}
 };
